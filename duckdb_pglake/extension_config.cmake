d8b25361d23e41e185fa8ceccfbd659800b6a4a4
David Christensen <david.christensen@snowflake.com>
2025-11-13 10:30:20 -0500

    Updates for duckdb 1.4.2

--- duckdb_pglake/extension_config.cmake
+++ duckdb_pglake/extension_config.cmake
@@ -1,7 +1,7 @@
 # External extensions to link into libduckdb
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 6b683b1d0b8df3596c65fea97952425ec9469d9a
+    GIT_TAG 39ebaf77e93a55b2bb839b621794eba49b2e359b
     INCLUDE_DIR src/include
     ADD_PATCHES
 )
