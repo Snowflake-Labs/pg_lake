fdae8fa0a0eb0fa33ca2427d7f8fda15bd7b209b
Onder KALACI <oender.kalaci@snowflake.com>
2026-01-15 08:33:32 +0300

    Fix pg_extension_base version
    It seems 3.2 is added accidentially, let's move it to 3.1

--- pg_extension_base/pg_extension_base--1.6--3.1.sql
+++ pg_extension_base/pg_extension_base--1.6--3.1.sql
@@ -1,2 +1,12 @@
 -- Upgrade script for pg_extension_base from 1.6 to 3.1
+/* remove a worker from another extension, by id */
+CREATE FUNCTION extension_base.deregister_worker(worker_id int)
+ RETURNS void
+ LANGUAGE c
+AS 'MODULE_PATHNAME', $function$pg_extension_base_deregister_worker$function$;
+
+COMMENT ON FUNCTION extension_base.deregister_worker(int)
+ IS 'deregister a base worker';
+
+REVOKE ALL ON FUNCTION extension_base.deregister_worker(int) FROM public;
 
