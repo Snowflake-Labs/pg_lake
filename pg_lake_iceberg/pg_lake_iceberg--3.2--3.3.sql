6127deeb8fdf79055857cfa4d8046e62fb384bf8
David Christensen <david.christensen@snowflake.com>
2026-03-11 10:07:28 -0400

    Modifications to iceberg metadata views
    Grant SELECT to public to lake_iceberg.tables*.
    
    Filter REST-based iceberg tables from the iceberg_tables view as they have no
    metadata file, and thus won't be useful to consumers.
    
    Signed-off-by: David Christensen <david.christensen@snowflake.com>

--- pg_lake_iceberg/pg_lake_iceberg--3.2--3.3.sql
+++ pg_lake_iceberg/pg_lake_iceberg--3.2--3.3.sql
@@ -5,3 +5,13 @@
 ALTER TABLE lake_iceberg.namespace_properties REPLICA IDENTITY FULL;
 ALTER TABLE lake_iceberg.tables_internal REPLICA IDENTITY FULL;
 ALTER TABLE lake_iceberg.tables_external REPLICA IDENTITY FULL;
+
+GRANT SELECT ON lake_iceberg.tables TO public;
+GRANT SELECT ON lake_iceberg.tables_internal TO public;
+GRANT SELECT ON lake_iceberg.tables_external TO public;
+
+CREATE OR REPLACE VIEW pg_catalog.iceberg_tables AS
+	SELECT catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location
+	FROM lake_iceberg.tables
+    WHERE metadata_location IS NOT NULL;
+
